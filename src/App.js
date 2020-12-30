7546af92bf4ae688d88d468d5c25c742a152aba0
pjmantoss <joelptoss@gmail.com>
2020-12-30 20:14:44 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
+import Timer from './components/Timer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Timer</h1>
+      <h1>25 + 5 Clock</h1>
     </div>
   );
 }
