2c3f9495eb3d93d34acf70c5942f6c05f7e4111d
pjmantoss <joelptoss@gmail.com>
2020-12-31 09:45:48 +0100

    update Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,10 +4,10 @@ import alarm from '../Alarm-Fast-A1.mp3';
 function Timer(props){
     return(
         <section id="timer">
-            <h2 id="timer-label"></h2>
-            <p id="timer-left"></p>
+            <h2 id="timer-label">{props.interval}</h2>
+            <p id="timer-left">{props.time}</p>
             <button id="start_stop" onClick={}></button>
-            <button id="reset" onClick={}></button>
+            <button id="reset" onClick={props.reset}></button>
         </section>
     )
 }
