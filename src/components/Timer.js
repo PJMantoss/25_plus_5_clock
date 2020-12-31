05848e3f9f3f2850a5faf3b82847c25ec082e722
pjmantoss <joelptoss@gmail.com>
2020-12-31 22:28:03 +0100

    add github pages and update packages.json and README

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,7 +5,7 @@ function Timer(props){
     return(
         <section id="timer">
             <h2 id="timer-label">{props.interval}</h2>
-            <p id="timer-left">{props.time}</p>
+            <p id="time-left">{props.time}</p>
             <button 
                 id="start_stop" 
                 onClick={props.isPaused ? props.start : props.pause}
