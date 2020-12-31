6f37ee3eda83160a2241f656559dc94a0109a565
pjmantoss <joelptoss@gmail.com>
2020-12-31 09:55:36 +0100

    update IntervalSetting

--- src/components/IntervalSetting.js
+++ src/components/IntervalSetting.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 function IntervalSetting(props){
+    const type = props.type;
+
     return(
         <section>
             <h2></h2>
-            <div></div>
+
+            <div>
+                <button id={} onClick={}></button>
+
+                <p></p>
+                
+                <button id={} onClick={}></button>
+            </div>
         </section>
     )
 }
