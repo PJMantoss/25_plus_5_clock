68bdb5f45cbdbc66947ce56a20d0cd4c7cbb7199
pjmantoss <joelptoss@gmail.com>
2020-12-31 13:01:11 +0100

    add styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React,{ useState } from 'react';
 import Timer from './components/Timer';
 import IntervalSetting from './components/IntervalSetting';
 // import Footer from './components/Footer';
