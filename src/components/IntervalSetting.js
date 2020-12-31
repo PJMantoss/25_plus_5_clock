3abd8ceecd784a28a960a5de12eca0ff53da3d66
pjmantoss <joelptoss@gmail.com>
2020-12-31 09:59:14 +0100

    update IntervalSetting

--- src/components/IntervalSetting.js
+++ src/components/IntervalSetting.js
@@ -8,11 +8,11 @@ function IntervalSetting(props){
             <h2 id={`${type}-label`}>{props.type}</h2>
 
             <div>
-                <button id={`${type}-decrement`} onClick={props.handleChange}></button>
+                <button id={`${type}-decrement`} onClick={props.handleChange}> &#9660; </button>
 
                 <p id={`${type}-length`}> {props.length} </p>
 
-                <button id={`${type}-increment`} onClick={props.handleChange}></button>
+                <button id={`${type}-increment`} onClick={props.handleChange}> &#9650; </button>
             </div>
         </section>
     )
